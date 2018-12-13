31943bf6588d624a08dff22d2464ee02f58e009a
tpluscode <tpluscode@users.noreply.github.com>
2018-12-13 19:06:17 +0100

    ignore errors

--- loading-overlay/loading-overlay.ts
+++ loading-overlay/loading-overlay.ts
@@ -3,20 +3,26 @@ import { IronOverlayBehavior } from '@polymer/iron-overlay-behavior/iron-overlay
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js'
 import { html, PolymerElement } from '@polymer/polymer/polymer-element'
 
+// @ts-ignore
 @customElement('loading-overlay')
 export default class LoadingOverlay extends mixinBehaviors(IronOverlayBehavior, PolymerElement) {
+    // @ts-ignore
     @property({ type: Boolean, readOnly: true })
     public readonly withBackdrop = true
 
+    // @ts-ignore
     @property({ type: Boolean, readOnly: true })
     public readonly noCancelOnOutsideClick = true
 
+    // @ts-ignore
     @property({ type: Boolean, readOnly: true })
     public readonly alwaysOnTop = true
 
+    // @ts-ignore
     @property({ type: Boolean, readOnly: true })
     public readonly noCancelOnEscKey = true
 
+    // @ts-ignore
     @property({ type: Boolean, readOnly: true })
     public readonly autoFitOnAttach = true
 
