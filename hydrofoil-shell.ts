9d1756bd76c3fc8cb5921ceab5a55494f8bb939b
tpluscode <tpluscode@users.noreply.github.com>
2019-05-07 13:39:40 +0200

    render 3 sections inside the shell for ready, error and loaded states

--- hydrofoil-shell.ts
+++ hydrofoil-shell.ts
@@ -141,7 +141,18 @@ export class HydrofoilShell extends LitElement {
                           base="${ifDefined(this.baseUrl)}"
                           client-base-path="${ifDefined(this.clientBasePath)}"
                           ?use-hash-fragment="${ifDefined(this.useHashUrls)}"></ld-navigator>
-            ${this.renderMain()}`
+
+            <section id="ready" ?hidden="${this.state !== 'ready'}">
+                <slot></slot>
+            </section>
+
+            <section id="main" ?hidden="${this.state !== 'loaded'}">
+                ${this.renderMain()}
+            </section>
+
+            <section id="error" ?hidden="${this.state !== 'error'}">
+                ${this.renderError()}
+            </section>`
     }
 
     /**
@@ -153,6 +164,15 @@ export class HydrofoilShell extends LitElement {
      */
     protected renderMain() {
         return html`<lit-view .value="${this.model}" ignore-missing template-scope="hydrofoil-shell"></lit-view>`
+    }
+
+    /**
+     * Renders the contents of last error was caught in the shell
+     *
+     * @returns {TemplateResult}
+     */
+    protected renderError() {
+        return html`<pre>${this.lastError && this.lastError.stack}</pre>`
     }
 
     private urlChanged(e: CustomEvent) {
